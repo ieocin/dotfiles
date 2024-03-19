2c893bfa131037a7c9cd0ac7ff7ccdc1cef88b22
nico <76963435+ieocin@users.noreply.github.com>
2024-03-19 01:24:13 +0100

    Update screenshot.sh

--- linux/scripts/screenshot.sh
+++ linux/scripts/screenshot.sh
@@ -1,9 +1,19 @@
-#!/bin/bash
+#!/bin/zsh
 name=$(date +"%d-%m-%y_%H:%M:%S")
-if [ "$1" == "-f" ]; then
-	maim ~/Pictures/$name.png
-else
-	maim -s ~/Pictures/$name.png
-fi
 
-xclip -selection clipboard -t image/png ~/Pictures/$name.png
+if [[ $XDG_SESSION_TYPE == "wayland" ]]; then
+	if [[ "$1" == "-f" ]]; then
+		echo a
+		wayshot -s -f ~/Pictures/$name.png -c
+	else
+		wayshot -f ~/Pictures/$name.png -c
+	fi
+	wl-copy < ~/Pictures/$name.png
+else
+	if [[ "$1" == "-f" ]]; then
+		maim ~/Pictures/$name.png
+	else
+		maim -s ~/Pictures/$name.png
+	fi
+	xclip -selection clipboard -t image/png ~/Pictures/$name.png
+fi
